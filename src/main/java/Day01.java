07112022eea4c39af3717461251bcb678f677107
seohyeon <binseohyeon718@gmail.com>
2025-01-02 16:50:22 +0900

    Day01: 입출력

---
+++ src/main/java/Day01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Day01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("이름을 입력해주세요: ");
+        String name = scanner.next();
+        System.out.println("헬로ㅂ");
+        System.out.println("name = " + name);
+//        System.out.println("hello world");
+    }
+}
