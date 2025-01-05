6265e78e68ba261695f5ed70f7846092f476cdfa
seohyeon <binseohyeon718@gmail.com>
2025-01-05 16:35:44 +0900

    Day02: 변수

---
+++ src/main/java/d02/Day02.java
@@ -0,0 +1,16 @@
+package d02;
+
+public class Day02 {
+    public static void main(String[] args) {
+        boolean isOld = false;
+        byte v1 = 3;
+        char ch = 'a';
+        short v2 = 2;
+        int v3 = 10;
+        long v4 = 2L;
+        float v5 = 3.0f;
+        double v6 = 4.0;
+        String name = "서현";
+    
+    }
+}
